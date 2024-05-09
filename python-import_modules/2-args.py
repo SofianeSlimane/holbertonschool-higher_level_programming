3d775301d54f869c522b1adb5fd350039fbcbb60
SofianeSlimane <sofianeslimane24@gmail.com>
2024-05-09 19:23:49 +0000

    [ADDED] Program that prints arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 import sys
-if len(sys.argv) == 1:
-    print("{} argument:".format(len(sys.argv)))
-    print("{}: {}".format(i, sys.argv[i]))
-elif len(sys.argv) == 0:
-    print("{} arguments.".format(len(sys.argv)))
-else:
-    print("{} arguments:".format(len(sys.argv) - 1))
-    for i in range(1, len(sys.argv)):
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("{} argument:".format(len(sys.argv)))
         print("{}: {}".format(i, sys.argv[i]))
+    elif len(sys.argv) == 0:
+        print("{} arguments.".format(len(sys.argv)))
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
