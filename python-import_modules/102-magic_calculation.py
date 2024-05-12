5f29a8a22b82750e2d6e824530eb321044cf5858
SofianeSlimane <sofianeslimane24@gmail.com>
2024-05-12 02:09:21 +0000

    [SAVING] work on bytecode

--- python-import_modules/102-magic_calculation.py
+++ python-import_modules/102-magic_calculation.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 import dis
-from magic_calculation_102 import add, sub
 
 def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
-        for i in range(90):
-            break
-
-print(dis(magic_calculation))
+        
+        for i in range(4 ,6):
+            c = add(c, i)
+        return c
+    elif not(a > b):
+        return sub(a, b)
+    return 
+print(dis.dis(magic_calculation))
