4596191b3362e7ab24ada70be4693a96ae11a2f5
SofianeSlimane <sofianeslimane24@gmail.com>
2024-05-14 13:10:53 +0000

    [MODIFIED] 0-square_matrix_simple: removed square function by ** operator

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,9 +2,8 @@
 def square_matrix_simple(matrix=[]):
     mx_ = len(matrix)
     new_matrix = [[0] * mx_ for i in range(mx_)]
-    def square(num): return num * num
     for i in range(mx_):
         for j in range(mx_):
-            value_squared = square(matrix[i][j])
+            value_squared = matrix[i][j] ** 2
             new_matrix[i][j] = value_squared
     return new_matrix
