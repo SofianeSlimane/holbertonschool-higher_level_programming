75c793ccdc587e15566044c66ce8b256633da55a
SofianeSlimane <sofianeslimane24@gmail.com>
2024-05-14 18:52:00 +0000

    [MODIFIED] 10-best_score: added key parameter to max function to retrieve key

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,5 +3,4 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     else:
-        a_dictionary['key_max'] = max(a_dictionary)
-        return a_dictionary['key_max']
+        return max(a_dictionary, key=a_dictionary.get)
