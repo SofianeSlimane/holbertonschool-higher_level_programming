77eb8219aabf58ade05b8623961a2531276ff9ce
SofianeSlimane <sofianeslimane24@gmail.com>
2024-05-14 14:27:37 +0000

    [MODIFIED] 0-square_matrix_simple: It nows adapt to any size of matrix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     mx_ = len(matrix)
-    new_matrix = [[0] * mx_ for i in range(mx_)]
+    numberOfColumns = len(matrix[0])
+    new_matrix = [[0] * numberOfColumns for i in range(mx_)]
+    print(new_matrix)
     for i in range(mx_):
-        for j in range(mx_):
+        for j in range(numberOfColumns):
             value_squared = matrix[i][j] ** 2
             new_matrix[i][j] = value_squared
+    print("len matrix : {}".format(mx_))
+    print("len new_matrix : {}".format(len(new_matrix)))
     return new_matrix
