50e2f43334276bab8d73fec0e09a9fc9d253a7b5
SofianeSlimane <sofianeslimane24@gmail.com>
2024-05-14 10:35:54 +0000

    [MODIFIED] 0-square_matrix_simple: Used def instead of lambda

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     mx_ = len(matrix)
-    new_matrix = [[0] * mx_] * mx_
-    square = lambda num: num * num
+    new_matrix = [[0] * mx_ for i in range(mx_)]
+    def square(num): return num * num
     for i in range(mx_):
         for j in range(mx_):
             value_squared = square(matrix[i][j])
             new_matrix[i][j] = value_squared
-    return new_matrix 
+    return new_matrix
