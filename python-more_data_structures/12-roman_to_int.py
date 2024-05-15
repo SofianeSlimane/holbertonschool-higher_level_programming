67534abfa63578ccf29cbad3493358fd20971545
SofianeSlimane <sofianeslimane24@gmail.com>
2024-05-15 11:11:32 +0000

    [MODIFIED] 12-roman_to_int: if stat: -2 sum

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -4,29 +4,21 @@ def roman_to_int(roman_string):
     sum_roman = 0
     i = 0
     while i < len(list_):
+        if list_[i] != "I" and list_[i - 1] == "I" and i - 1 >= 0:
+            sum_roman -= 2
         if list_[i] == "X":
             sum_roman += 10
-            if list_[i - 1] == "I":
-                sum_roman -= 2
         if list_[i] == "V":
-                sum_roman += 5
+            sum_roman += 5
         if list_[i] == "L":
             sum_roman += 50
-            if list_[i - 1] == "I":
-                sum_roman -= 2
         if list_[i] == "C":
             sum_roman += 100
-            if list_[i - 1] == "I":
-                sum_roman -= 2
         if list_[i] == "M":
             sum_roman += 1000
-            if list_[i - 1] == "I":
-                sum_roman -= 2
         if list_[i] == "I":
-                sum_roman += 1
+            sum_roman += 1
         if list_[i] == "D":
             sum_roman += 500
-            if list_[i -1] == "I":
-                sum_roman -= 2
         i += 1
     return sum_roman
