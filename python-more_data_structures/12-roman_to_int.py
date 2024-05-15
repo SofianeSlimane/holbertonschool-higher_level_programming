d150d3d9d7be1616f8def01b352dbe0907963fab
SofianeSlimane <sofianeslimane24@gmail.com>
2024-05-15 12:41:44 +0000

    [MODIFIED] 12-roman_to_int: Added condition if roman_string is str

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None:
+    if roman_string is None or type(roman_string) != str:
         return 0
     list_ = list(roman_string)
     sum_roman = 0
