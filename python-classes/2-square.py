9d4e8e9de1e369b8b0cdf70b0806a685fddd1ea6
SofianeSlimane <sofianeslimane24@gmail.com>
2024-05-17 22:41:39 +0000

    [ADDED] 2-square: Defines a square and exceptions for different values of size

---
+++ python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module contains the square class"""
+
+
+class Square:
+    """Defines a square and its size.
+    Attributes:
+        size: integer that represents square size.
+    """
+    def __init__(self, size=0):
+        """Initializes a square using size.
+
+            Args:
+             size: Square size with default value of 0
+             """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
