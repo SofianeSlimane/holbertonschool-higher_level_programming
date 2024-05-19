8d2c987f82317fbe2b9bed0d12d4d8dbb39fc458
SofianeSlimane <sofianeslimane24@gmail.com>
2024-05-19 08:40:53 +0000

    [MODIFIED] 6-square -> position.setter: changed if statement

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -81,5 +81,9 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         elif value[0] < 0 and value[0] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
+        elif isinstance(value[0], int) is False:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif isinstance(value[1], int) is False:
+            raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
