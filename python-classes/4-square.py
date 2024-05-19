018fd91f187ce61b153bd723c626db2348dedd39
SofianeSlimane <sofianeslimane24@gmail.com>
2024-05-19 01:40:58 +0000

    [ADDED] 4-square: Created a Square class with getter ans setter methods using decorators

---
+++ python-classes/4-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Defines a square class."""
+
+
+class Square:
+
+    def __init__(self, size=0):
+        """Initializes a Square object.
+        Args:
+            size: square size with default value of zero
+        """
+        self.__size = size
+
+    def area(self):
+        """Computes the area of the square object
+        Returns:
+            int: area value
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Retrieves private instance attribute size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Changes size attribute value if necessary
+        Args:
+            value: square size
+
+        Raises:
+            TypeError: if size is not an integer
+            ValueError: if size value is negative
+        """
+        if isinstance(value, int) is False:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
