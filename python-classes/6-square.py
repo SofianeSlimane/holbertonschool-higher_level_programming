90e3f1bb4763edd883f7d98fc7ad9a358776dc0b
SofianeSlimane <sofianeslimane24@gmail.com>
2024-05-19 03:28:47 +0000

    [ADDED] 6-square: Complete Square class that prints Square object at given position

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -43,9 +43,6 @@ class Square:
     def size(self):
         """Retrieves private instance attribute size"""
         return self.__size
-    @property
-    def position(self):
-        return self.__position
 
     @size.setter
     def size(self, value):
@@ -63,9 +60,22 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
-    
+
+    @property
+    def position(self):
+        """Retrieves private instance attribute position"""
+        return self.__position
+
     @position.setter
     def position(self, value):
+        """Set position value
+        Args:
+            value: tuple of the Square object coordinates
+
+        Raises:
+            TypeError: value is not a tuple or an element is a negative integer
+
+        """
         if isinstance(value, tuple) is False or value[0] <= 0 or value[1] <= 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
