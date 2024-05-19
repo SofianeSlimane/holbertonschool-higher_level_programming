771b76ed035ad22b381c63f20ec71b24c203cfc6
SofianeSlimane <sofianeslimane24@gmail.com>
2024-05-19 14:58:50 +0000

    [MODIFIED] 6-square: removed conditions for testing purposes

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -54,9 +54,7 @@ class Square:
 
         """
 
-        if (isinstance(value, tuple) is False or len(value) != 2
-            or isinstance(value[0], int) is False or isinstance(value[1], int)
-                is False or value[0] < 0 or value[1] < 0):
+        if isinstance(value, tuple) is Fals²e or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
