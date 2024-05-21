ae3a7e3f4969a118d3b425517d42499f1303c782
SofianeSlimane <sofianeslimane24@gmail.com>
2024-05-21 11:31:41 +0000

    [MODIFIED] 3-say_my_name: Added documentation

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""This module ocntains the say_my_name function"""
+
+
 def say_my_name(first_name, last_name=""):
+    """ This function prints a first and last name
+    Args:
+        first_name = first name
+        last_name = last name
+    Raises:
+        TypeError: if one of parameter is not a string
+    """
     if isinstance(first_name, str) is False:
         raise TypeError("first_name must be a string")
     elif isinstance(last_name, str) is False:
