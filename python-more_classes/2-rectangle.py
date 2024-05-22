f8ef1aed269a47289d7afbc621c01d534055f399
SofianeSlimane <sofianeslimane24@gmail.com>
2024-05-22 14:37:47 +0000

    [ADDED] 2-rectangle.py: Class methods: area and perimeter

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+"""This module contains the Rectangle class"""
+
+
+class Rectangle:
+    """Rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        """Initializes a Rectangle object
+        Args:
+            width: width
+            height: height
+        """
+        self.width = width
+        self.height = height
+
+    def area(self):
+        """Computes the area of the Rectangle object"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Computes the perimeter of the Rectangle object"""
+        perimeter = 0
+        if self.__width == 0 or self.__height == 0:
+            perimeter = 0
+        else:
+            perimeter = (self.__width + self.__height) * 2
+
+        return perimeter
+
+    @property
+    def width(self):
+        """Retrieves private instance attribute width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Changes width attribute value if necessary
+        Args:
+            value: Rectangle width
+
+        Raises:
+            TypeError: if width is not an integer
+            ValueError: if width value is negative
+        """
+        if isinstance(value, int) is False:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """Retrieves private instance attribute height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Changes size attribute value if necessary
+        Args:
+            value: Rectangle height
+
+        Raises:
+            TypeError: if height is not an integer
+            ValueError: if height value is negative
+        """
+        if isinstance(value, int) is False:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
