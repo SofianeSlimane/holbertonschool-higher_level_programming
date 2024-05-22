cfe6374ded9695720e17234973df84c89e039025
SofianeSlimane <sofianeslimane24@gmail.com>
2024-05-22 10:25:59 +0000

    [MODIFIED] 6-max_integer.py: Added a lot of tests

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -9,3 +9,11 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([]), None)
     def test_max_int(self):
         self.assertEqual(max_integer([100, 50, 0]), 100)
+    def test_max_negative_int(self):
+        self.assertEqual(max_integer([-1, -10, -100]), -1)
+    def test_max_neg_pos_int(self):
+        self.assertEqual(max_integer([50, -1, -15]), 50)
+    def test_max_string(self):
+        self.assertEqual(max_integer(["The", "One", "Piece", "is", "real"]), "real")
+    def test_max_bool(self):
+        self.assertEqual(max_integer([True, False]), True)
