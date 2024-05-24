fac4adfd97cadd1d9a654adcab1c4d870a892c56
SofianeSlimane <sofianeslimane24@gmail.com>
2024-05-24 15:51:30 +0000

    [MODIFIED] 8-rectangle.py: Importing 7-base_geometry module instead of re-writing BaseGeometry

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,29 +1,6 @@
 #!/usr/bin/python3
 """ This module contains the BaseGeometry class """
-
-
-class BaseGeometry:
-    """ This is the BaseGeometry class """
-    def area(self):
-        """ This is the area method which
-        only raises an exception for now
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ This method validates value.
-        Args:
-            name: name associated with the value.
-            value: value
-        Raise:
-            TypeError: if value is not an integer.
-            ValueError: if value is inferior or equal
-            to 0
-        """
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        elif value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
