d245c5c7c796cd235ff3c333d5b0a46b7d562260
SofianeSlimane <sofianeslimane24@gmail.com>
2024-05-24 12:40:36 +0000

    [MODIFED] 7-base_geometry.py: returns exception with formatted message, 7-base_geometry.txt: Added a lot of tests

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -21,6 +21,6 @@ class BaseGeometry:
             to 0
         """
         if not isinstance(value, int):
-            raise TypeError("<name> must be an integer")
+            raise TypeError(f"{name} must be an integer")
         elif value <= 0:
-            raise ValueError("<name> must be greater than 0")
+            raise ValueError(f"{name} must be greater than 0")
