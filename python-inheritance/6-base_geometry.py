59aef68db92e98b8c7886489e99b703bcc3a7a08
SofianeSlimane <sofianeslimane24@gmail.com>
2024-05-24 11:46:20 +0000

    [ADDED] 6-base_geometry.py: defined public instance method area(), that only raises an exception when it is called

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" This module contains the BaseGeometry class """
+
+
+class BaseGeometry:
+    """ This is the BaseGeometry class """
+    def area(self):
+        """ This is the area method which
+        only raises an exception for now
+        """
+        raise Exception("area() is not implemented")
