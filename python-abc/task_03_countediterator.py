6ff04ebefbc3e206ab59486e54dbfc6590d320ea
SofianeSlimane <sofianeslimane24@gmail.com>
2024-05-26 13:01:37 +0000

    [ADDED] task_03_countediterator.py: created a class that extends the built-in iterator obtained from the iter function

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" This module contains the CountedIterator class """
+
+
+class CountedIterator:
+    """ The CountedIterator class is a
+    class that extends the built-in iterator obtained from
+    the iter function.
+    """
+    def __init__(self, some_iterable):
+        """ Initializes object.
+        Args:
+            some_iterable: iterable to iterate over
+            counter: number of items that we iterated over
+        """
+        self.iterator = iter(some_iterable)
+        self.counter = 0
+
+    def get_count(self):
+        """ Returns the current value of the counter """
+        return self.counter
+
+    def __next__(self):
+        """ Returns the next item in the iterable """
+        self.counter += 1
+        return next(self.iterator)
