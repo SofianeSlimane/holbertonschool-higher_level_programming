bbc707fd865019a1fc374e567062e9fed3b4cb4c
SofianeSlimane <sofianeslimane24@gmail.com>
2024-05-26 17:44:39 +0000

    [ADDED] task_05_dragon.py: Created two mixins and a class that inherits from both of these mixins

---
+++ python-abc/task_05_dragon.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" This module contains the SwimMixin and FlyMixin and
+a subclass Dragon that inherits from both.
+"""
+
+
+class SwimMixin:
+    """ SwimMixin mixin """
+    def swim(self):
+        """ swim method """
+        print("The creature swims!")
+
+class FlyMixin:
+    """ FlyMixin mixin """
+    def fly(self):
+        """ fly method """
+        print("The creature flies!")
+
+class Dragon(SwimMixin, FlyMixin):
+    """ Dragon subclass that inherits from two
+    mixins.
+    """
+    def roar(self):
+        """ roar method unique to Dragon """
+        print("The dragon roars!")
+draco = Dragon()
+draco.swim()
+draco.fly()
+draco.roar()
