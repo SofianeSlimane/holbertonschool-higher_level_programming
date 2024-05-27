ea27e72e24b64237f6decbafd893e46049903c1f
SofianeSlimane <sofianeslimane24@gmail.com>
2024-05-27 10:46:33 +0000

    [ADDED] 0-read_file.py: Function that read and print content from a file + README

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" This module contains the read_file function """
+
+
+def read_file(filename=""):
+    """ This function reads the content of a file
+    given as a parameter.
+    Args:
+        filename: name of the file to read from
+    """
+    with open(filename, 'r', encoding="utf-8") as myFile:
+        read_data = myFile.read()
+        print(read_data)
