0067b0330bf097591fde87176dee617199a77430
SofianeSlimane <sofianeslimane24@gmail.com>
2024-05-27 15:19:57 +0000

    [MODIFIED] 5-save_to_json_file.py: Removing encoding again

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,5 +6,5 @@ import json
 def save_to_json_file(my_obj, filename):
     """This function saves an object in a text file in its JSON representation.
     """
-    with open(filename, 'w', encoding="utf-8") as myFile:
+    with open(filename, 'w') as myFile:
         json.dump(my_obj, myFile)
