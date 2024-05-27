2cb7c8f00d31ef5203171eaab1e4877cf9b9e9f5
SofianeSlimane <sofianeslimane24@gmail.com>
2024-05-27 14:59:02 +0000

    [MODIFIED] 5-save_to_json_file.py: Added documentation back, not the problem

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -4,5 +4,11 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
+    """This function saves an object in 
+    a text file, in its JSON representation.
+    Args:
+        my_obj: object to be saved in file
+        filename: file
+    """
     with open(filename, 'w') as myFile:
         json.dump(my_obj, myFile)
