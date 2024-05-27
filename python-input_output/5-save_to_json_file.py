9f5f9119a3cfedb530547e18ae6e9af50f2cd309
SofianeSlimane <sofianeslimane24@gmail.com>
2024-05-27 15:26:08 +0000

    [MODIFIED] 5-save_to_json_file.py: Documentation

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-""" This module contains the function save_to_json_file """
-
-
+"""This module contains the function save_to_json_file"""
 import json
 
 
 def save_to_json_file(my_obj, filename):
     """This function saves an object in a text file in its JSON representation.
+    Args:
+        my_obj: object to be saved in file
+        filename: file
     """
     with open(filename, 'w') as myFile:
         json.dump(my_obj, myFile)
