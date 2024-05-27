64f9027ab9ef78766fbda2b7c1c6035200c51ee0
SofianeSlimane <sofianeslimane24@gmail.com>
2024-05-27 14:56:24 +0000

    [MODIFIED] 5-save_to_json_file.py: Removing documentation for checker testing

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -4,12 +4,5 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-    """ This function writes an object to a text file.
-    The dump function first serializes the Python object into
-    JSON representation, and then saves it in the given file name.
-    Args:
-        my_obj: object to be written into file
-        filename: file to write object into
-    """
     with open(filename, 'w') as myFile:
         json.dump(my_obj, myFile)
