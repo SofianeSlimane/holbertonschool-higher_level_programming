c6a6d49052877ddf9b0afea60905a236928937a9
SofianeSlimane <sofianeslimane24@gmail.com>
2024-05-28 08:44:58 +0000

    [ADDED] 7-add_item.py: Script that adds all arguments to a Pyton list, and save them in a file using JSON representation

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module contains a script that adds all arguments
+to a list and save them them to a file. The list is then
+retrieved as a Python Object.
+"""
+import sys, json
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+my_list = sys.argv
+my_list.pop(0)
+save_to_json_file(my_list, "add_item.json")
+my_python_object = load_from_json_file("add_item.json")
