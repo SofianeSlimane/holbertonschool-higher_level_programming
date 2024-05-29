b04acb08c8fb291f2728ae6a6c959c32eb521fee
SofianeSlimane <sofianeslimane24@gmail.com>
2024-05-29 10:11:15 +0000

    [ADDED] 12-pascal_triangle.py: Function that returns the Pascal's triangle representation of an integer

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
-
-
+"""This module contains the pascal_triangle function"""
 
 
 def pascal_triangle(n):
+    """This function returns a matrix of integers
+    representing the Pascal's triangle of n.
+    Args:
+        n: number of rows in the triangle
+    Returns: Pascal triangle matrix
+    """
     matrix = []
+    if n <= 0:
+        return matrix
+
     for i in range(1, n + 1):
-        matrix.append([0] * i)
-    print(matrix)
+        matrix.append([1] * i)
+    
+    for i in range(n):
+        if not len(matrix[i]) <= 2:
+            for j in range(1, len(matrix[i]) - 1):
+                matrix[i][j] = matrix[i - 1][j - 1] + matrix[i - 1][j]
+
+    return matrix
