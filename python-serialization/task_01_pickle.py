157125b60cdf8a1d74613d24f39aa8e36d199920
SofianeSlimane <sofianeslimane24@gmail.com>
2024-05-30 17:27:18 +0000

    [ADDED] task_01_pickle.py: CustomObject class, pickle module

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""This module contains the CustomObject class"""
+import pickle
+
+
+class CustomObject:
+    """Custom Object Class."""
+    def __init__(self, name, age, is_student):
+        """Constructor method.
+        Args:
+            name: name
+            age (int): age
+            is_student (bool): determines if object is student
+        """
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def serialize(self, filename):
+        """Converts an object into a binary
+        stream and writes it in a file.
+        Args:
+            filename: file binary stream will be
+            written to.
+        Returns: None if object cannot be serialized
+        or, if the file is not found.
+        """
+        try:
+            with open(filename, "wb") as myFile:
+                pickle.dump(self, myFile)
+        except pickle.PicklingError:
+            return None
+        except FileNotFoundError:
+            return None
+
+    def display(self):
+        """Prints a formated object's
+        attribute dictionary.
+        """
+        for key, value in self.__dict__.items():
+            print("{}: {}".format(key, value))
+
+    @classmethod
+    def deserialize(cls, filename):
+        """Converts a binary stream back into a
+        Python object.
+        Returns: None if object cannot be serialized
+        or, if the file is not found.
+        """
+        try:
+            with open(filename, "rb") as myFile:
+                my_object = pickle.load(myFile)
+                return my_object
+        except FileNotFoundError:
+            return None
+        except pickle.UnpicklingError:
+            return None
