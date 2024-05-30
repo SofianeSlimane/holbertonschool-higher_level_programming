51912782abeaabfc390176d10a50b58a644ec3d6
SofianeSlimane <sofianeslimane24@gmail.com>
2024-05-30 13:13:17 +0000

    [MODIFIED] 7-add_item.py: handled FileNotFoundError

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,10 +8,13 @@ import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 my_list = []
+for elements in sys.argv:
+    my_list.append(elements)
+my_list.pop(0)
+
 try:
-    my_list = load_from_json_file("add_item.json")
+    my_python_object = load_from_json_file("add_item.json")
 except FileNotFoundError:
-    for elements in sys.argv:
-        my_list.append(elements)
-        my_list.pop(0)
-        save_to_json_file(my_list, "add_item.json")
+    save_to_json_file(my_list, "add_item.json")
+
+save_to_json_file(my_list, "add_item.json")
