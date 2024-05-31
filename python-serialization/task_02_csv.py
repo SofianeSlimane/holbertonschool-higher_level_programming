5113b4e2388cf4df4785804d804c703e98d35923
SofianeSlimane <sofianeslimane24@gmail.com>
2024-05-31 15:52:00 +0000

    [ADDED]  task_02_csv.py: Function that converts cvs data to json, still have to work on printing format

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""This module contains the convert_cvs_to_json function"""
+import csv
+import json
+
+
+def convert_csv_to_json(csv_file):
+    """Converts cvs data to json.
+    Args:
+        cvs_file: cvs file containing the data
+    Returns: True if conversion was successful, else, false
+    """
+    my_list = []
+    try:
+        with open(csv_file, "r", encoding="utf-8") as myFile:
+            csvReader = csv.DictReader(myFile)
+            for rows in csvReader:
+                my_list.append(rows)
+        with open("data.json", "w", encoding="utf-8") as myFile:
+            json.dump(my_list, myFile, indent=1)
+    except FileNotFoundError:
+        return False
+    except AttributeError:
+        return False
+    except EOFError:
+        return False
+    except ImportError:
+        return False
+    except IndexError:
+        return False
+    except cvs.Error:
+        return False
