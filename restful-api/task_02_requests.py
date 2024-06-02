3738c843f4c561a52ad3910ee3b97b6260340d76
SofianeSlimane <sofianeslimane24@gmail.com>
2024-06-02 14:51:56 +0000

    [ADDED] task_02_requests.py: Added userId back in columns

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -60,12 +60,12 @@ def fetch_and_save_posts():
         my_list = eval(my_response.text)
         # print(type(my_list))
         # print(my_list[0])
-        for dictionary in my_list:
-            del dictionary['userId']
+        # for dictionary in my_list:
+        #    del dictionary['userId']
         # print(my_list)
-        fieldnames = ["id", "title", "body"]
+        fieldnames = ["userId", "id", "title", "body"]
 
-        with open("posts.csv", 'w', newline='') as csvfile:
+        with open("posts.csv", 'w') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
             writer.writerows(my_list)
