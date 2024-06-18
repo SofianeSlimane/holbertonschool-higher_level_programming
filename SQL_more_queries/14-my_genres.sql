bf185f054717cc54353c898655034798e557186f
SofianeSlimane <sofianeslimane24@gmail.com>
2024-06-19 00:40:31 +0200

    [MODIFIED] 14-my_genres.sql: mentionning show_id in more dynamic way, also specifying dexter title

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,5 @@
 -- List genre base on both matching show id and genre id
 SELECT name
-FROM tv_genres, tv_show_genres
-WHERE tv_show_genres.show_id = 8 AND tv_show_genres.genre_id = tv_genres.id
+FROM tv_genres, tv_show_genres, tv_shows
+WHERE tv_show_genres.show_id = tv_shows.id AND tv_show_genres.genre_id = tv_genres.id AND tv_shows.title = 'Dexter'
 ORDER BY name ASC;
