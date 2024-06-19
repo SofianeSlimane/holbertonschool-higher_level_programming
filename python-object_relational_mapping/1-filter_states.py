d3b241855a74d1281f4f96360232ac8b1957543c
SofianeSlimane <sofianeslimane24@gmail.com>
2024-06-19 22:27:29 +0200

    [ADDED] 1-filter_states.py: Same as task 0 but with like operator

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module connects to MySQL database and lists all
+all rows from a table where name starts with a N"""
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+    my_sql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    db = MySQLdb.connect(host='localhost', user=my_sql_username,
+                         passwd=mysql_password, db=database_name,
+                         port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT * from states WHERE name
+                LIKE 'N%' ORDER BY states.id ASC""")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
