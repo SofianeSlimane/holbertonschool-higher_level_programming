31730c753bdcb6e6bd698c91cdb747b9b23f2b0f
SofianeSlimane <sofianeslimane24@gmail.com>
2024-06-20 01:09:25 +0200

    [ADDED] 3-my_safe_filter_states.py: same as task 2  but safe from SQL injection

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""This module connects to MySQL database and lists all
+specific rows from a table, safe from SQL injection"""
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+    if len(sys.argv) > 5:
+        exit
+    else:
+        my_sql_username = sys.argv[1]
+        mysql_password = sys.argv[2]
+        database_name = sys.argv[3]
+        state_name_searched = sys.argv[4]
+
+    db = MySQLdb.connect(host='localhost', user=my_sql_username,
+                         passwd=mysql_password, db=database_name,
+                         port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states WHERE BINARY name = '{}' ORDER
+                BY states.id ASC""".format(state_name_searched))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
