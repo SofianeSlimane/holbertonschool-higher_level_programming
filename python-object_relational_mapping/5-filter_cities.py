fcc3b2102508770446deb0e34fe8963b9995dc5e
SofianeSlimane <sofianeslimane24@gmail.com>
2024-06-20 11:45:04 +0200

    [ADDED] 5-filter_cities.py: lists all cities that belong to a state

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""This module connects to MySQL database and lists all
+specific rows from a table, safe from SQL injection"""
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+    if len(sys.argv) > 5 or ";" in sys.argv[4]:
+        raise ValueError
+    else:
+        my_sql_username = sys.argv[1]
+        mysql_password = sys.argv[2]
+        database_name = sys.argv[3]
+        state_name = sys.argv[4]
+        i = 0
+    db = MySQLdb.connect(host='localhost', user=my_sql_username,
+                         passwd=mysql_password, db=database_name,
+                         port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT cities.name FROM cities,
+                 states WHERE states.name = '{}' AND states.id =
+                 cities.state_id ORDER BY cities.id ASC""".format(state_name))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row[0], end="")
+        if i < len(rows) - 1:
+            print(", ", end="")
+        i += 1
+    print()
+    # print(sys.argv)
+    # print(len(sys.argv))
+    cur.close()
+    db.close()
