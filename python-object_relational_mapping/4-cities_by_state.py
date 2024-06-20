523680be677f72906856241b9e07f40b2bb7e274
SofianeSlimane <sofianeslimane24@gmail.com>
2024-06-20 02:04:50 +0200

    [ADDED] 4-cities_by_state.py: list cities

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module connects to MySQL database and lists all
+all rows from a table"""
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+    my_sql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    db = MySQLdb.connect(host='localhost', user=my_sql_username,
+                         passwd=mysql_password, db=database_name,
+                         port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * from cities ORDER BY cities.id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
