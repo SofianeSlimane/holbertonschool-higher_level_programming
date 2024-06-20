45a7263f9455855bef3b7bf3586b1bcc90af516a
SofianeSlimane <sofianeslimane24@gmail.com>
2024-06-20 02:10:09 +0200

    [MODIFIED] 4-cities_by_state.py: input validation

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python3
 """This module connects to MySQL database and lists all
-all rows from a table"""
+specific rows from a table, safe from SQL injection"""
 if __name__ == "__main__":
     import MySQLdb
     import sys
-    my_sql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
+    if len(sys.argv) > 4 or ";" in sys.argv[3]:
+        raise ValueError
+    else:
+        my_sql_username = sys.argv[1]
+        mysql_password = sys.argv[2]
+        database_name = sys.argv[3]
+
     db = MySQLdb.connect(host='localhost', user=my_sql_username,
                          passwd=mysql_password, db=database_name,
                          port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * from cities ORDER BY cities.id ASC")
+    cur.execute("""SELECT * FROM cities
+                ORDER BY cities.id ASC""")
     rows = cur.fetchall()
     for row in rows:
         print(row)
-
+    # print(sys.argv)
+    # print(len(sys.argv))
     cur.close()
     db.close()
