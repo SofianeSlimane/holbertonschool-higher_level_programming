6a23b02a29a062c770c42f612a4357fe6e3805f4
SofianeSlimane <sofianeslimane24@gmail.com>
2024-06-20 22:13:15 +0200

    [ADDED] 9-model_state_filter_a.py: retrieves rows where name contains 'a'

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This script allows us to connect to a MySQL database,
+thanks to create_engine, sessionmaker creates an interface
+that allows us to interacte with the database and do
+operations like retrieving rows from a table, thanks
+to Session's query method"""
+if __name__ == '__main__':
+    import sys
+    from sqlalchemy import create_engine
+    from model_state import Base, State
+    from sqlalchemy.orm import sessionmaker
+
+    my_sql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+                            my_sql_username, mysql_password,
+                            database_name), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session_object = Session()
+    only_a_rows = session_object.query(State).filter(
+        State.name.contains('a')).order_by(State.id)
+    for instance in only_a_rows:
+        print(f"{instance.id}: {instance.name}")
