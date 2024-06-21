0799baf54a01325f9753857946885c1c2e1f731f
SofianeSlimane <sofianeslimane24@gmail.com>
2024-06-21 20:56:32 +0200

    [ADDED] 11-model_state_insert.py: inserts new row

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This script allows us to connect to a MySQL database,
+thanks to create_engine, sessionmaker creates an interface
+that allows us to interacte with the database and do
+operations like retrieving rows from a table, thanks
+to Session's query method"""
+if __name__ == '__main__':
+    import sys
+    from sqlalchemy import create_engine, insert
+    from model_state import Base, State
+    from sqlalchemy.orm import sessionmaker
+
+    for arguments in sys.argv:
+        if ";" in arguments:
+            raise ValueError
+        my_sql_username = sys.argv[1]
+        mysql_password = sys.argv[2]
+        database_name = sys.argv[3]
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+                            my_sql_username, mysql_password,
+                            database_name), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session_object = Session()
+    louisiana = State(name="Louisiana")
+    session_object.add(louisiana)
+    session_object.commit()
