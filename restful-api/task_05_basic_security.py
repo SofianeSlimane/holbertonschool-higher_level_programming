8ba3c356db053c8f85b491cfe64587718d9cfb26
SofianeSlimane <sofianeslimane24@gmail.com>
2024-06-23 14:17:25 +0200

    Removed task 2

---
+++ restful-api/task_05_basic_security.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from flask import Flask
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+users = {
+    "john": generate_password_hash("hello"),
+    "susan": generate_password_hash("bye")
+}
