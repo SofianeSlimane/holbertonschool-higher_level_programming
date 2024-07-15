4c655357224960fe1e86446649b9872dbc0452ca
SofianeSlimane <sofianeslimane24@gmail.com>
2024-07-15 12:01:01 +0200

    Corrected some error handlers, placed error handlers at beginning of code

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -13,20 +13,24 @@ def generate_invitations(template, attendees):
             where each value replaces its respective value
             in template.
             """
-    index = 1
-    file_path = "output_X.txt"
-    new_str = template
     if type(template) is not str:
         raise TypeError(f"{type(template)} is invalid")
+
     elif type(attendees) is not list:
         raise TypeError(f"{type(attendees)} is invalid")
+
+    elif len(template) == 0:
+        raise ValueError("Template is empty, no output files generated.")
+
+    elif len(attendees) == 0:
+        raise ValueError("No data provided, no output files generated.")
+
     for items in attendees:
         if type(items) is not dict:
             raise TypeError("attendees must be a list of dictionnaries")
-    if len(template) == 0:
-        raise ValueError("Template is empty, no output files generated")
-    elif len(attendees) == 0:
-        raise ValueError("No data provided, no output files generated.")
+
+    index = 1
+    new_str = template
 
     for people in attendees:
         for key, value in people.items():
