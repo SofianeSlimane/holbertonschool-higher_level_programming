1557bd5b0686b40cd15f2c8ea888a737caa5d1db
SofianeSlimane <sofianeslimane24@gmail.com>
2024-07-15 13:53:50 +0200

    using print statement instead of raise statement

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -13,6 +13,9 @@ def generate_invitations(template, attendees):
             where each value replaces its respective value
             in template.
             """
+    if len(template) == 0:
+        print("Template is empty, no output files generated.")
+        exit()
 
     for items in attendees:
         if type(items) is not dict:
